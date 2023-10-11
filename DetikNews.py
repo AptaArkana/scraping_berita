419967b02a826c986187496678f89814f5be4d7d
Rangga Saputra <79633073+AptaArkana@users.noreply.github.com>
2023-10-11 19:09:38 +0700

    Update DetikNews.py

--- DetikNews.py
+++ DetikNews.py
@@ -12,76 +12,35 @@ class DetikNewsApi:
 
     def build_search_url(self, query: str, page_number: int):
         """Untuk mencari berita yang sesuai dengan query dan mengambil sesuai dengan halamannya"""
-        qs = f'query={query}'
-        qs2 = '&siteid=2&sortby=time&sorttime=0&page='
-        return self.search_url + qs + qs2 + str(page_number)
-
-    def build_detail_url(self, url: str):
-        """Url detailnya"""
-        a = urlsplit(url)
-        qs = 'single=1'
-        detail_url = a.scheme + '://' + a.netloc + a.path + '?' + qs
-        return detail_url
-
-    def result_count(self, search_response):
-        """Jumlah pencarian"""
-        soup = BeautifulSoup(search_response.text, 'html.parser')
-        tag = soup.find('span', 'fl text').text
-        count = [int(s) for s in tag.split() if s.isdigit()]
-        return count[0]
-
-    def detail(self, url: str) -> str:
-        """Inti berita"""
-        detail_url = self.build_detail_url(url)
-        req = get(detail_url)
-        soup = BeautifulSoup(req.text, 'html.parser')
-        tag = soup.find('div', class_="detail__body-text")
-        body = ''
-        if tag.find_all('p'):
-            for i in tag.find_all('p'):
-                body += i.text
+        if page_number == 1:
+            return f"{self.search_url}query={query}&siteid=2"
         else:
-            body += tag.text
-        return body
+            return f"{self.search_url}query={query}&siteid=2&sortby=time&sorttime=0&page={page_number}"
 
-    def parse(self, search_response, detail):
-        """Inti scraping"""
-        soup = BeautifulSoup(search_response.text, 'html.parser')
-        tag = soup.find_all('article')
+    def scrape_articles(self, query: str, max_pages: int):
         data = []
 
-        for i in tag:
-            judul = i.find('h2').text
-            link = i.find('a').get('href')
-            gambar = i.find('img').get('src')
-            body = ''
-            if detail:
-                body = self.detail(link)
-            waktu = i.find('span', class_="date").text
-            time_scr = datetime.date.today().strftime("%Y-%m-%d")
-            data.append({'judul': judul,
-                    'link': link,
-                    'gambar': gambar,
-                    'body': body,
-                    'waktu': waktu,
-                    'waktu_scraping': time_scr,
-                    'berita_hoax': False
-                    })
-        return pd.DataFrame(data)
+        for page in range(1, max_pages + 1):
+            url = self.build_search_url(query, page)
 
-    def search(self, query, num_pages=1, detail=False):
-        """Fungsi untuk scrapings"""
-        data = []
+            # Request URL Website
+            page = get(url)
 
-        for page_number in range(1, num_pages + 1):
-            url = self.build_search_url(query, page_number)
-            search_response = get(url)
-            parse_result = self.parse(search_response, detail)
-            data.append(parse_result)
+            # Parsing format HTML ke dalam BeautifulSoup
+            soup = BeautifulSoup(page.text, "html.parser")
 
-        # Menggabungkan hasil dari semua halaman ke dalam satu DataFrame
-        if data:
-            combined_data = pd.concat(data, ignore_index=True)
-            return combined_data
-        else:
-            return pd.DataFrame()
+            # Memanggil judul artikel yang berada di luar
+            article = soup.findAll("article")
+
+            # Mengambil judul artikel dan link
+            for scraping in article:
+                text = scraping.find("h2", "title").text.strip().split("\n")
+                link = scraping.find("a")["href"]
+                keterangan = scraping.find("span", "category")
+                waktu = datetime.date.today().strftime("%Y-%m-%d")
+
+                data.append([text[0], link, keterangan, waktu, False])  # Memasukkan data scraping ke dalam list data
+
+        # Membuat DataFrame
+        df = pd.DataFrame(data, columns=["Judul Artikel", "Link", "Team", "Waktu_Scraping", "Berita_Hoax"])
+        return df
